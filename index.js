1707454058703d88014343d9da9f71145deb96d0
zubair ahmad <75520334+za7019223@users.noreply.github.com>
2021-08-16 00:15:35 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// Write a js program to print day of week name using switch case.
+
+// var day = mon;
+// switch (day) {
+//     case "mon":
+//         console.log("today is mon");
+//         break;
+//     case "tue":
+//         console.log("today is tue");
+//         break;
+//     case "wed":
+//         console.log("today is wed");
+//         break;
+//     case "thu":
+//         console.log("today is thu");
+//         break;
+//     case "sat":
+//         console.log("today is sat");
+//         break;
+//     case "sun":
+//         console.log("today is sun");
+//         break;
+
+//     default:
+//         break;
+// }
